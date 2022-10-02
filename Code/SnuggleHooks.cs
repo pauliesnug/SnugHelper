98bc9a686ccb838266d79e44decce96d6a407373
Ethan <git@ethanlibs.co>
2022-10-01 20:12:23 -0400

    suf

--- Code/SnuggleHooks.cs
+++ Code/SnuggleHooks.cs
@@ -4,16 +4,8 @@ namespace Celeste.Mod.SnugHelper;
 
 public static class SnuggleHooks {
     public static void Load() {
-        On.Celeste.Achievements.Register += Achievements_Register;
     }
 
     public static void Unload() {
-        On.Celeste.Achievements.Register -= Achievements_Register;
-    }
-
-    private static void Achievements_Register(On.Celeste.Achievements.orig_Register orig, Achievement achievement) {
-        foreach (Achievement ach in (Achievement[]) Enum.GetValues(typeof(Achievement))) {
-            orig(ach);
-        }
     }
 }
